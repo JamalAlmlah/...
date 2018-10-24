03da80ec7880f2c2ff1e9799cf56b2b5840ac76f
Jamal Al Mlah <9907jaal@res.ludvika.intra>
2018-10-24 09:04:17 +0200

    ignore config.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,16 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+const connection = new WebSocket('ws://104.248.143.87:1337');
+
+connection.onopen = () => {
+console.log('uppkoppling started ... ');
+
+}
+connection.onerror = error => {
+  console.log(`Error: ${error}`);
+}
+connection.onmessage = message => {}
+connection.onclose = () => {
+console.log('upploppling nedstängd ...')
+}
